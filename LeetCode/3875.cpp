9da1bce1522bee284a6827f2b6c9381b092b85da
757-ishant <ishantchauhan757@gmail.com>
2026-09-25 21:20:40 +0530

    new ques

---
+++ LeetCode/3875.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+    }
+int main(){
+    vector<int> nums1={1,2,3,4};
+    cout<<uniformArray(nums1)<<endl;
+}
