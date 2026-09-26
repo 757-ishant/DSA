6817dde3fcfd3efdeb6348d450b98936303c9197
757-ishant <ishantchauhan757@gmail.com>
2026-09-26 15:27:36 +0530

    new ques

---
+++ LeetCode/3718.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+int missingMultiple(vector<int>& nums, int k) {
+    unordered_set<int> st(nums.begin(), nums.end());
+
+    int multiple = k;
+
+    while (st.count(multiple)) {
+        multiple += k;
+    }
+
+    return multiple;
+}
+
+int main() {
+    vector<int> nums = {2, 4, 6};
+    int k = 2;
+
+    cout << missingMultiple(nums, k) << endl;
+
+    return 0;
+}
